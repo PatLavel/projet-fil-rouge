fc08955c9983bb004a43b16e2b9c11efc25c61be
Jeremie Melchior <melchiorjeremie@gmail.com>
2021-05-24 12:11:29 +0200

    creation script ajout nouvel utilisateur

---
+++ Controller/script_ajout_compte.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Nemelade : inscription</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" />
+
+</head>
+
+<body>
+    <?php
+
+    // if (!isset($_SESSION['email'])) {
+    //     header('Location: form_connexion.php');
+    // }
+    $hash = password_hash($_POST['user_password'], PASSWORD_DEFAULT);
+
+    Ajout_utilisateur($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['login'], $_POST['user_password'], $hash);
+
+    function Ajout_utilisateur($nom, $prenom, $mail, $login, $password, $hash)
+    {
+        $mysqli = new mysqli('127.0.0.1', 'root', '', 'nemelade');
+        $stmt = $mysqli->prepare("INSERT INTO utilisateur (id, nom, prenom, mail, argent, login, password, hash)
+        SELECT MAX(id)+1, ?, ?, ?, 100, ?, ?, ? FROM utilisateur;");
+        $stmt->bind_param("ssssss", $nom, $prenom, $mail, $login, $password, $hash);
+        $stmt->execute();
+        $mysqli->close();
+    }
+
+    echo "<div class='insert_account'>
+                <div><p>Votre compte a été créé avec succès.</p></div>
+                <div><a class='btn btn-dark btn-sm' href='Connexion.html'>valider</a></div>
+                </div>";
+
+    ?>
+</body>
+
+</html>
