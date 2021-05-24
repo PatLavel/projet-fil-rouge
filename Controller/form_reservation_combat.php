1f8fc52c6fd4c01b9ee03822809ed0a07b5b6052
Jeremie Melchior <melchiorjeremie@gmail.com>
2021-05-24 17:15:22 +0200

    modif pour la reservation de places

--- Controller/form_reservation_combat.php
+++ Controller/form_reservation_combat.php
@@ -8,7 +8,7 @@ session_start();
 <head>
 
     <meta charset="utf-8" />
-    <title>Nemelade : modifications</title>
+    <title>Nemelade : reservation</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" />
 
@@ -39,43 +39,39 @@ session_start();
         </div>
     </header>
 
-    <h1 class="myh1">Bienvenue chez vous.</h1>
-    <h2 class="myh2">Vous pouvez modifier vos informations personnelles. </h2>
+    <h1 class="myh1">Envie d'un peu d'action?</h1>
+    <h2 class="myh2">Assistez à un des combats de l'arène!</h2>
+
+    <?php
+    $result = userEarnMoney($_SESSION['user_login']);
+
+    function userEarnMoney($login)
+    {
+
+        $mysqli = new mysqli('127.0.0.1', 'root', '', 'nemelade');
+        $stmt = $mysqli->prepare("SELECT round(argent) FROM utilisateur WHERE login=?;");
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+        $rs = $stmt->get_result();
+        $tab = $rs->fetch_array(MYSQLI_ASSOC);
+        $rs->free();
+        $mysqli->close();
+        return $tab;
+    }
+    ?>
+
 
     <div class="subscription">
-        <div class="picture">
-            <img class="profile_pic" title="user_profile_pic" src="user_pic.PNG" alt="user_profile_pic" />
-            <input type="submit" class="button2" value="Modifier photo">
-        </div>
         <div class="subscription_form">
-            <form method="POST">
-                <p class="myp">Entrez votre nouvelle adresse email. Vous recevrez un email de confirmation sur votre
-                    nouvelle adresse.</p>
-                <input class="case" type="email" name="email" placeholder="Nouvelle adresse email">
-                <input class="case" type="email" name="confirm_email" placeholder="Confirmer nouvelle adresse email">
-                <p class="myp">Vous pouvez changer de pseudo mais attention, les autres citoyens pourraient ne pas vous
-                    reconnaitre!</p>
-                <input class="case" type="text" name="pseudo" placeholder="Nouveau pseudo">
-                <p class="myp">Vous pouvez renouveler le sort de protection de votre compte en changeant de mot de
-                    passe.</p>
-                <input class="case" type="password" name="password" placeholder="Mot de passe actuel"><br>
-                <input class="case" type="password" name="password" placeholder="Nouveau mot de passe">
-                <input class="case" type="password" name="confirm_password" placeholder="Confirmer nouveau mot de passe">
-                <div>
-                    <input type="checkbox" id="newsletter" name="newsletter">
-                    <label for="newsletter">Je veux recevoir la Gazette de Nemelade par email</label>
-                </div>
-                <div>
-                    <input type="submit" class="button" value="Enregistrer">
-                </div>
-            </form>
+            <p class="myp">Vous disposez actuellement de <?php echo $result['round(argent)'] ?> PO.</p>
+            <p class="myp">Êtes vous certain de vouloir dépenser 10 PO pour assister à ce match?</p>
+            <div class="validation">
+            </div>
+            <input type="submit" class="button" value="Je réserve ma place!" href="">
+
         </div>
     </div>
 
-
-    <div class="bottom">
-
-    </div>
     <div class="footer">
         <div class="left">
             <img class="logo_footer" title="logo_blanc" src="logo_blanc.PNG" alt="logo_nemelade" />
