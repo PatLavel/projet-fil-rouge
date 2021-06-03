fbdb74d58982dd30ec489c4700d348d0df6cf123
debu_remi <remi.debu@free.fr>
2021-06-03 16:49:12 +0200

    control button creation sujet

--- Controller/Projet_Creation_Sujet.php
+++ Controller/Projet_Creation_Sujet.php
@@ -3,10 +3,13 @@ session_start();
 include_once("../Service/TopicService.php");
 include_once("../View/CreationSujetView.php");
 
-if (isset($_POST["poster"])) {
-    $topic = (new Topic())->setTitre($_POST["title"])->setMessage($_POST["message"]);
-    (new TopicService())->addTopic($topic);
-    header("Location: Projet_Forum.php");
+if (isset($_SESSION["user_login"])) {
+    if (isset($_POST["poster"])) {
+        $topic = (new Topic())->setTitre($_POST["title"])->setMessage($_POST["message"]);
+        (new TopicService())->addTopic($topic);
+        header("Location: Projet_Forum.php");
+    }
+    displayTopicCreation();
+} else {
+    header("Location: projet_forum.php");
 }
-
-displayTopicCreation();
