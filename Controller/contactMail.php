0f6cc4e3afea6c03315024f61eb4a833c1eb0522
lavelle <p.lavelle@numericable.fr>
2021-06-04 08:46:00 +0200

    paris

--- Controller/contactMail.php
+++ Controller/contactMail.php
@@ -8,9 +8,14 @@ if (isset($_POST['Mail'])) {
     $Sujet = $_POST['Sujet'];
     $message = $_POST['msg'];
     $name = $_POST['Nom'] . $_POST['Prenom'];;
-    echo ($admin_email . $email . $Sujet . $message . $name);
+   // echo ($admin_email . $email . $Sujet . $message . $name);
     //send email  
-    mail($admin_email, $Sujet, $message);
+    
+    if (mail($admin_email, $Sujet, $message)) {
+        echo "Email successfully sent to $admin_email...";
+    } else {
+        echo "Email sending failed...";
+    }
     //Email response   , "From:" . $email
 }
 //if "email" variable is not filled out, display the form   
