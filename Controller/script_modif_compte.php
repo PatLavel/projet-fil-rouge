58de99eeb3e03ac3bc9c07553c6e8a9f7826b3a7
Jeremie Melchior <melchiorjeremie@gmail.com>
2021-06-13 15:02:54 +0200

    suppression commentaire

--- Controller/script_modif_compte.php
+++ Controller/script_modif_compte.php
@@ -11,12 +11,3 @@ if (!isset($_SESSION['email'])) {
 $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 updateUser($_POST['email'], $_POST['password'], $hash, $_POST['id']);
-
-// function updateUser($mail, $password, $hash, $id)
-// {
-//     $mysqli = new mysqli('127.0.0.1', 'root', '', 'nemelade');
-//     $stmt = $mysqli->prepare("UPDATE utilisateur SET mail = ?, password = ?, hash= ? WHERE id=?;");
-//     $stmt->bind_param("sssi", $mail, $password, $hash, $id);
-//     $stmt->execute();
-//     $mysqli->close();
-// }
