6cf11b90a4b184e691f1b85bc97c6ead64bb44b3
Jeremie Melchior <melchiorjeremie@gmail.com>
2021-06-13 09:41:13 +0200

    résolution pb footer issue #47

--- Controller/modification_compte.php
+++ Controller/modification_compte.php
@@ -3,9 +3,11 @@ session_start();
 include_once(__DIR__ . "/../view/CommonView.php");
 include_once(__DIR__ . "/../service/evenementService.php");
 include_once(__DIR__ . "/../service/UtilisateurService.php");
+include_once(__DIR__ . "/../DAO/")
 
 ?>
 
+// commence ici
 <!DOCTYPE html>
 <html>
 
