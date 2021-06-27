ed32404488272953cc00238c4641dce220b21fc4
Jeremie Melchior <melchiorjeremie@gmail.com>
2021-06-27 21:19:35 +0200

    correction erreur syntaxe

--- DAO/evenementDAO.php
+++ DAO/evenementDAO.php
@@ -17,7 +17,8 @@ class EvenementDAO extends CommonDAO
         foreach ($row as $value) {
 
             //var_dump($value["id"]) ;
-            $evenement = (new Evenement())->setId($value["id"])->setLibelle($value["libelle"])->setDateDebut($value["dateDebut"])->setDateFin($value["dateFin"])->setCombattant1($value["combattant1"])->setCombattant2($value["combattant2"])->setImgMatch($value["img"]);
+            $evenement = (new Evenement())->setId($value["id"])->setLibelle($value["libelle"])->setDateDebut($value["dateDebut"])
+                ->setDateFin($value["dateFin"])->setCombattant1($value["combattant1"])->setCombattant2($value["combattant2"])->setImgMatch($value["img"]);
             $tabEv[] = $evenement;
         }
         //$result->free();
@@ -33,7 +34,8 @@ class EvenementDAO extends CommonDAO
         $stat->execute(['id' => $id]);
         $row = $stat->fetch();
         //var_dump($row) ;
-        $ev = (new Evenement())->setId($row["id"])->setLibelle($row["libelle"])->setDateDebut($row["dateDebut"])->setDateFin($row["dateFin"])->setCombattant1($row["combattant1"])->setCombattant2($row["combattant2"]);
+        $ev = (new Evenement())->setId($row["id"])->setLibelle($row["libelle"])->setDateDebut($row["dateDebut"])
+            ->setDateFin($row["dateFin"])->setCombattant1($row["combattant1"])->setCombattant2($row["combattant2"]);
 
         return $ev;
     }
@@ -45,7 +47,8 @@ class EvenementDAO extends CommonDAO
     {
 
         $mysqli = new mysqli('127.0.0.1', 'root', '', 'nemelade');
-        $stmt = $mysqli->prepare("INSERT INTO reservation (id, numero, date, nombrePlace, counterPlace, id_utilisateur, id_evenement) SELECT MAX(id)+1, MAX(numero)+1, sysdate(), 1, MAX(counterPlace)+1, ?, ? FROM reservation;");
+        $stmt = $mysqli->prepare("INSERT INTO reservation (id, numero, date, nombrePlace, counterPlace, id_utilisateur, id_evenement) SELECT MAX(id)+1, 
+        MAX(numero)+1, sysdate(), 1, MAX(counterPlace)+1, ?, ? FROM reservation;");
         $stmt->bind_param("ii", $idUser, $idCombat);
         $stmt->execute();
         $mysqli->close();
